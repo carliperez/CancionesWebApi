8cdc153796a305569b7819c12c8342c346c77266
Carla <a2020118629@estudiantes.upsa.edu.bo>
2021-10-13 13:28:50 -0400

    Migrations implementados

---
+++ CancionesWebApi/Migrations/20211013172624_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CancionesWebApi.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cancion",
+                columns: table => new
+                {
+                    NombreCancion = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Autor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Letra = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Link = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cancion", x => x.NombreCancion);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cancion");
+        }
+    }
+}
