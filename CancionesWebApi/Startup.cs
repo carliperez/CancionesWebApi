c5a5764c40b5f28dbb8d79cd46fae0d21f66a7fc
Carla <a2020118629@estudiantes.upsa.edu.bo>
2021-10-13 13:23:11 -0400

    Db Context agregado a startup

--- CancionesWebApi/Startup.cs
+++ CancionesWebApi/Startup.cs
@@ -1,7 +1,9 @@
+using CancionesWebApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,10 @@ namespace CancionesWebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CancionesWebApi", Version = "v1" });
             });
+
+            services.AddDbContext<AppDbContext>(options =>
+                       options.UseSqlServer(Configuration.GetConnectionString("CarlaContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
