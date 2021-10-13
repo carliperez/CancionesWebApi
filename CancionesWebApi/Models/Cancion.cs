799a7c042722267b1aa730694eeb0a2de0671dfd
Carla <a2020118629@estudiantes.upsa.edu.bo>
2021-10-13 11:43:28 -0400

    Agregar archivos de proyecto.

---
+++ CancionesWebApi/Models/Cancion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancionesWebApi.Models
+{
+    public class Cancion
+    {
+        [Key]
+        public string NombreCancion { get; set; }
+        [Required(ErrorMessage = "El campo del nombre es requerido")]
+        public string Autor { get; set; }
+        [Required(ErrorMessage = "El campo del nombre es requerido")]
+        [StringLength(200, MinimumLength = 5, ErrorMessage = "El nombre tiene que tener entre 5 a 20 caracteres")]
+        public string Letra { get; set; }
+        [Url]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "El nombre tiene que tener entre 2 a 100 caracteres")]
+        public string Link { get; set; }
+
+    }
+}
